2a54fe08275645fd437e9390f7dcaeaf16383da3
N3STOR12 <l21211920@tectijuana.edu.mx>
2022-05-19 11:54:49 -0700

    Trabajo Clase
    Clase excepciones
    Clase: 19/05/2022

---
+++ excepciones-cSharp/excepciones-cSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            try
+            {
+                Console.Write("Introduce un numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Introduce otro numero: ");
+               int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+
+
+        }
+    }
+}
